44e28f4d99100d1d2a5cb10048e26c4175500c3d
Lukacim02 <lukacim49@gmail.com>
2024-11-11 17:40:37 +0200

    actual

--- dz/dz.2.2.ry.py
+++ dz/dz.2.2.ry.py
@@ -1,9 +1,10 @@
 
-number = 78964
-reversed_number = 0
-while number > 0:
-    reversed_number = reversed_number * 10 + number % 10
-    number = number // 10
+number = int(input("Введіть 5-значне число: "))
+digit1 = number % 10
+digit2 = (number // 10) % 10
+digit3 = (number // 100) % 10
+digit4 = (number // 1000) % 10
+digit5 = (number // 10000) % 10
+reversed_number = digit1 * 10000 + digit2 * 1000 + digit3 * 100 + digit4 * 10 + digit5
 print(reversed_number)
-
 
