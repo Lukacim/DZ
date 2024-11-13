bfb06ae1cf673c579563ce356a100fc38d0e2e6e
Lukacim <lukacim49@gmail.com>
2024-11-13 18:34:33 +0200

    Add files via upload

---
+++ dz/dz3.1.py
@@ -0,0 +1,24 @@
+def calculator():
+
+    num1 = float(input("Введіть перше число: "))
+    operation = input("Введіть операцію (+, -, *, /): ")
+    num2 = float(input("Введіть друге число: "))
+
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == '/':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            return "Помилка: ділення на нуль!"
+    else:
+        return "Невірна операція!"
+
+    return f"Результат: {result}"
+
+
+print(calculator())
