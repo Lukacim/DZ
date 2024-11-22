32e177fdcf768bb04926123e99fae9674f7af35f
Lukacim <lukacim49@gmail.com>
2024-11-22 14:56:55 +0200

    Update dz6.3.py

--- dz/dz6.3.py
+++ dz/dz6.3.py
@@ -1,15 +1,9 @@
-
 num = input("Введіть число: ")
-
-
 while int(num) > 9:
-    product = 1
-
+    number = 1
     for digit in num:
-        product *= int(digit)
+        number *= int(digit)
 
-    num = str(product)
-
+    num = str(number)
 
 print(num)
-
