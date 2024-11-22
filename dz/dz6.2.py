c91dacb4e655c2f1e199d440b96245bf8198d594
Lukacim <lukacim49@gmail.com>
2024-11-22 14:35:32 +0200

    Update dz6.2.py

--- dz/dz6.2.py
+++ dz/dz6.2.py
@@ -1,11 +1,9 @@
-# Введення кількості секунд
+
 seconds = int(input("Введіть кількість секунд: "))
 
-# Перевірка, чи число в межах дозволеного діапазону
 if seconds < 0 or seconds >= 8640000:
     print("Число повинно бути від 0 до 8639999 включно.")
 else:
-    # Розрахунок кількості днів, годин, хвилин і секунд
     days = seconds // (24 * 60 * 60)
     remaining_seconds = seconds % (24 * 60 * 60)
     hours = remaining_seconds // (60 * 60)
@@ -13,7 +11,8 @@ else:
     minutes = remaining_seconds // 60
     seconds = remaining_seconds % 60
 
-    # Визначення правильного закінчення для слова "день"
+
+    
     if days == 1:
         day_word = "день"
     elif 2 <= days <= 4:
@@ -21,7 +20,8 @@ else:
     else:
         day_word = "днів"
 
-    # Форматований результат
+
+    
     print(f"{days} {day_word}, {str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}")
 
 
