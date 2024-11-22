0ac0bde98c0efca3349371e872e0e28c8c139ecf
Lukacim <lukacim49@gmail.com>
2024-11-22 14:19:28 +0200

    Update dz6.1py.py

--- dz/dz6.1py.py
+++ dz/dz6.1py.py
@@ -1,6 +1,6 @@
 import string
 
-start, end = "a", "c" 
+start, end = input("Введіть дві літери через дефіс (наприклад, 'a-c'): ").split('-')
 
 all_letters = string.ascii_letters
 
@@ -8,4 +8,3 @@ start_index = all_letters.index(start)
 end_index = all_letters.index(end) + 1
 
 print(all_letters[start_index:end_index])
-
