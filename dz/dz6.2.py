4c8d2eaaa6f526bb3f9158fe838ed866b8773c8a
Lukacim <lukacim49@gmail.com>
2024-11-24 11:56:52 +0200

    Update dz6.2.py

--- dz/dz6.2.py
+++ dz/dz6.2.py
@@ -1,27 +1,14 @@
 
 seconds = int(input("Введіть кількість секунд: "))
+days = seconds // 86400
+hours = (seconds % 86400) // 3600
+minutes = (seconds % 3600) // 60
+seconds = seconds % 60
 
-if seconds < 0 or seconds >= 8640000:
-    print("Число повинно бути від 0 до 8639999 включно.")
+if days % 10 == 1 and days % 100 != 11:
+    day_word = "день"
+elif 2 <= days % 10 <= 4 and (days % 100 < 10 or days % 100 >= 20):
+    day_word = "дні"
 else:
-    days = seconds // (24 * 60 * 60)
-    remaining_seconds = seconds % (24 * 60 * 60)
-    hours = remaining_seconds // (60 * 60)
-    remaining_seconds = remaining_seconds % (60 * 60)
-    minutes = remaining_seconds // 60
-    seconds = remaining_seconds % 60
-
-
-    
-    if days == 1:
-        day_word = "день"
-    elif 2 <= days <= 4:
-        day_word = "дні"
-    else:
-        day_word = "днів"
-
-
-    
-    print(f"{days} {day_word}, {str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}")
-
-
+    day_word = "днів"
+print(f"{days} {day_word}, {str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}")
