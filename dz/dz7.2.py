1b34c1f2a84ace1f6d765ef7c1ecf2f5b5c94087
Lukacim <lukacim49@gmail.com>
2024-11-26 13:54:07 +0200

    Add files via upload

---
+++ dz/dz7.2.py
@@ -0,0 +1,16 @@
+
+print('ОК')
+def correct_sentence(text):
+    text = text[0].upper() + text[1:]  # Перша літера велика
+    if text[-1] != '.':               # Перевіряємо, чи останній символ — крапка
+        text += '.'
+    return text
+
+# Тести
+assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
+assert correct_sentence("hello") == "Hello.", 'Test2'
+assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
+assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
+assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
+
+print('ОК')
