53ed7bc61021fe7d2f31b36e484db03cfbcc18c6
Lukacim <lukacim49@gmail.com>
2024-11-28 11:46:56 +0200

    Update dz7.3.py

--- dz/dz7.3.py
+++ dz/dz7.3.py
@@ -9,8 +9,8 @@ def second_index(text, some_str):
     if second_index == -1:
         return None
     return second_index
-assert second_index("sims", "s") == 3, 'Test1'  # друге "s" знаходиться на індексі 3
-assert second_index("find the river", "e") == 12, 'Test2'  # друге "e" знаходиться на індексі 12
-assert second_index("hi", "h") is None, 'Test3'  # другого входження "h" немає
-assert second_index("Hello, hello", "lo") == 10, 'Test4'  # друге "lo" знаходиться на індексі 10
+assert second_index("sims", "s") == 3, 'Test1' 
+assert second_index("find the river", "e") == 12, 'Test2'  
+assert second_index("hi", "h") is None, 'Test3'  
+assert second_index("Hello, hello", "lo") == 10, 'Test4' 
 print('ОК')
