c408ef1f648b0bf0ecffcf5f97d0181205278803
Lukacim <lukacim49@gmail.com>
2024-11-28 13:14:33 +0200

    Update dz5.1.py

--- dz/dz5.1.py
+++ dz/dz5.1.py
@@ -1,26 +1,28 @@
-
 import string
 import keyword
 
-def is_valid_variable_name(var_name):
 
-    if not var_name:
+def is_valid_variable(name):
+
+    if name[0].isdigit():
         return False
 
-    if var_name[0].isdigit():
+    if any(char.isupper() for char in name):
         return False
 
-    if any(char.isupper() for char in var_name):
+    if any(char in string.punctuation and char != '_' for char in name):
         return False
 
-    if any(char in string.punctuation.replace("_", "") for char in var_name) or " " in var_name:
+    if name in keyword.kwlist:
         return False
 
-    if var_name in keyword.kwlist:
-        return False
-
-    if var_name.count("_") > 1:
+    if name.count('_') > 1:
         return False
 
     return True
 
+print(is_valid_variable("_"))  # True
+print(is_valid_variable("get_value"))  # True
+print(is_valid_variable("get value"))  # False
+print(is_valid_variable("assert"))  # False
+print(is_valid_variable("3m"))  # False
