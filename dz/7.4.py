5065841915b96bc72c2b17676f6869c45f626e5f
Lukacim <lukacim49@gmail.com>
2024-11-28 12:58:51 +0200

    Update 7.4.py

--- dz/7.4.py
+++ dz/7.4.py
@@ -1,8 +1,11 @@
 def common_elements():
-    # Знаходимо перетин множин без додаткових змінних
-    return {x for x in range(100) if x % 3 == 0} & {x for x in range(100) if x % 5 == 0}
 
-# Перевірка
-assert common_elements() == {0, 75, 45, 15, 90, 60, 30}
+    set_3 = {x for x in range(100) if x % 3 == 0}
+    set_5 = {x for x in range(100) if x % 5 == 0}
+
+    return set_3 & set_5
+
+assert common_elements() == {0, 15, 30, 45, 60, 75, 90}
+
 
 
