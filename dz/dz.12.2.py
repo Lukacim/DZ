099e51b87d51d42fa109d12c53551b10c647224a
Lukacim <lukacim49@gmail.com>
2024-12-13 17:39:26 +0200

    Update dz.12.2.py

--- dz/dz.12.2.py
+++ dz/dz.12.2.py
@@ -23,26 +23,24 @@ class Purchase:
         self.user = user
 
     def add_item(self, item, cnt):
-        
-        if item in self.products:
-            self.products[item] += cnt
-        else:
-            self.products[item] = cnt
+       
+        self.products[item] = cnt
 
     def get_total(self):
-   
+     
         return sum(item.price * cnt for item, cnt in self.products.items())
 
     def __str__(self):
         items_str = "\n".join(f"{item.name}: {cnt} pcs." for item, cnt in self.products.items())
-        return f"User: {self.user}\nItems:\n{items_str}\nTotal: {self.get_total()}"
+        return f"User: {self.user}\nItems:\n{items_str}"
+
 
 lemon = Item('lemon', 5, "yellow", "small")
 apple = Item('apple', 2, "red", "middle")
 print(lemon)  
 
-buyer = User("Ivan", "Ivanov", "0963495665")
-print(buyer)  # Ivan Ivanov
+buyer = User("Ivan", "Ivanov", "02628162")
+print(buyer) 
 
 cart = Purchase(buyer)
 cart.add_item(lemon, 4)
@@ -53,17 +51,16 @@ User: Ivan Ivanov
 Items:
 lemon: 4 pcs.
 apple: 20 pcs.
-Total: 60
 """
 assert isinstance(cart.user, User), 'Екземпляр класу User'
 assert cart.get_total() == 60, "Всього 60"
+
 cart.add_item(apple, 10)
 print(cart)
 """
 User: Ivan Ivanov
 Items:
 lemon: 4 pcs.
-apple: 30 pcs.
-Total: 80
+apple: 10 pcs.
 """
-assert cart.get_total() == 80, 'Повинно залишатися 80!'
+assert cart.get_total() == 40, 'Повинно залишатися 40!'
