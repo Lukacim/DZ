b8c40ea122b6f3f7ade45c142792951663d8b9f0
Lukacim <lukacim49@gmail.com>
2024-12-20 08:15:19 +0200

    Update 14.1.dz.py

--- dz/14.1.dz.py
+++ dz/14.1.dz.py
@@ -1,10 +1,9 @@
-# Визначаємо виняток користувача
+
 class GroupLimitExceededError(Exception):
     def __init__(self, message="Неможливо додати більше 10 студентів до групи"):
         self.message = message
         super().__init__(self.message)
 
-# Клас для групи
 class Group:
     def __init__(self):
         self.students = []
@@ -13,13 +12,10 @@ class Group:
         if len(self.students) >= 10:
             raise GroupLimitExceededError()
         self.students.append(student_name)
-
-# Створення об'єкта групи
 group = Group()
 
-# Додавання студентів, з обробкою винятку
 try:
-    for i in range(11):  # Спробуємо додати 11 студентів
+    for i in range(11): 
         group.add_student(f"Студент {i + 1}")
 except GroupLimitExceededError as e:
     print(f"Помилка: {e}")
